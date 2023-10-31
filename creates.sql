6780616143a5852db52b251ae6b88206cb80cbf1
CR4ZYZG <gabrielc.ftn1@gmail.com>
2023-10-30 22:25:57 -0300

    pequena mudança sql

--- creates.sql
+++ creates.sql
@@ -23,20 +23,12 @@ create table lotes(
 	id serial primary key,
 	cod integer not null,
 	galpao integer references galpoes(id),
-	aviculor integer references avicultores(id)
-);
-
-create table tecnicos(
-	id serial primary key,
-	nome varchar(200) not null,
-	email varchar(100) not null,
-	senha varchar(20) not null
+	avicultor integer references avicultores(id)
 );
 
 create table semanas(
 	id serial primary key,
 	semana numeric(1),
 	lote integer references lotes(id),
-	galpão integer references galpoes(id)
+	galpao integer references galpoes(id)
 );
-
