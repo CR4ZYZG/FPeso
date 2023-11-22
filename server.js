a27ab4d37d0081c2c908f738c84e79fcb4d302ad
CR4ZYZG <gabrielc.ftn1@gmail.com>
2023-11-22 19:06:29 -0300

    .

--- server.js
+++ server.js
@@ -1,53 +1,13 @@
-import { fastify } from 'fastify'
-//import { DatabaseMemory } from './database-memory.js'
-import { DatabasePostgres } from './database-postgres.js'
+import fastify from 'fastify'
+import AvicultorSrv from './src/AvicultorSrv.js'
 const server = fastify()
 
+const Avicultor = AvicultorSrv
+Avicultor(server)
 
-const database = new DatabasePostgres()
-
-
-server.get('/avicultores', async (request) =>{
-    const search = request.query.search
-    
-    const avicultores = await database.list(search)
-    
-    return avicultores
-})
-
-server.post('/avicultores', async (request, reply) =>{
-    const {nome, email, senha} = request.body
-
-    await database.create({
-        nome,
-        email,
-        senha,
-    })
-
-    return reply.status(201).send()
-})
-
-server.put('/avicultores/:id', async (request, reply) =>{
-    const {nome, email, senha} = request.body
-
-    const avicultorId = request.params.id
-
-    await database.update(avicultorId, {
-        nome,
-        email,
-        senha,
-    })
-
-    return reply.status(204).send()
-})
-
-server.delete('/avicultores/:id', async (request, reply) =>{
-    const avicultorId = request.params.id
-
-    await database.delete(avicultorId)
-
-    return reply.status(204).send()
-})
+//server.get('/', (req,res) =>{
+  //  server.login
+//})
 
 server.listen({
     host: '0.0.0.0',
---
+++ src/AvicultorSrv.js
@@ -0,0 +1,48 @@
+import {sql} from '../db.js'
+
+
+export default (server) => {
+    
+    server.get('/avicultores', async (search, request) =>{
+        var avicultores = ''
+        if (search){
+
+            avicultores = await sql`SELECT * FROM avicultores where nome ilike ${'%'+ search + '%'}`
+
+            return avicultores
+        } else {
+
+            avicultores = await sql`SELECT * FROM avicultores`
+
+            return avicultores
+        }
+            
+        
+    })
+    
+    server.post('/avicultores', async (request, reply) =>{
+        const {nome, email, senha} = request.body
+
+        await sql`insert into avicultores(nome, email, senha) VALUES (${nome}, ${email}, ${senha})`
+    
+        return reply.status(201).send()
+    })
+    
+    server.put('/avicultores/:id', async (id, request, reply) =>{
+        const {nome, email, senha} = request.body
+    
+        await sql`update avicultores set nome = ${nome}, email = ${email}, senha = ${senha} WHERE id = ${id}`
+    
+        return reply.status(204).send()
+    })
+    
+    server.delete('/avicultores/:id', async (request, reply) =>{
+        const avicultorId = request.params.id
+    
+        await database.delete(avicultorId)
+    
+        return reply.status(204).send()
+    })
+
+}
+
